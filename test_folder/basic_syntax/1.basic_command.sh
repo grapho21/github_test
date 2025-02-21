1c9c48f32c564ae70f98ea6269e31b6c2dc051e6
grapho21 <grapho21@hotmail.com>
2025-02-21 15:57:39 +0900

    version4

--- test_folder/basic_syntax/1.basic_command.sh
+++ test_folder/basic_syntax/1.basic_command.sh
@@ -1,3 +1,9 @@
 
 # git add . 은 모든 변경 사항을 staging area 이동
 git add .
+
+# git commit은 staging area의 변경 사항을 확정짓고 commitID 생성
+git commit -m "메시지"
+
+# 원격 repo에 main브랜치에 변경사항 (commitID)를 업로드
+git push origin main
